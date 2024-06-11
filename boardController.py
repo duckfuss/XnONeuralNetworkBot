12a4a8a987644badbcf766fa9aa647339d11100c
duckfuss <dtfussey@icloud.com>
2024-06-11 17:47:40 +0100

    generate a board

--- boardController.py
+++ boardController.py
@@ -1,5 +1,9 @@
+import numpy as np
+class Board():
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+        self.boardState = np.zeros((self.width, self.height))
+    def check(self):
+        print(self.boardState)
 
-class board():
-    def __init__(self) -> None:
-        
-        pass

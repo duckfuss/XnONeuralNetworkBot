fixed delta calculation

network.py
@@ -31,7 +31,8 @@ class Network():
             # loop over every other boardstate, (state 1 always goes first)
             boardState = boardHist[i][0]    # array of board state
             move = boardHist[i][1]          # tuple (row,col)
-            delta = (0.5*maths.exp(0.2*((i-len(boardHist))/2)))+0.5
+            x = (i-(self.turn-1))/2 # maps i to 0,1,2,3...
+            delta = (0.5*maths.exp(0.2*(x-len(boardHist))))+0.5
             # see https://www.desmos.com/calculator/owfq7vaas8
             
             compressed = np.divide(boardState, 2)
@@ -49,10 +50,10 @@ class Network():
                 desired[move[0]][move[1]] = delta
             if verbose:
                 print("\n\nboardState: \n", boardState)
-                print("output: \n", output)
+                print("output: \n", output, move)
                 print("desired: \n", desired)
-                print("delta:", delta, " turn:", self.turn, " move", move)
-                print("boardHIst:\n", boardHist)
+                print("delta:", delta, " lenBH:", len(boardHist))
+                print("turn:", self.turn, " i:", i, " x:", x)
 
             # calculate cost
             #  -> cost = self.cost(output,desired)